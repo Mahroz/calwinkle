949629af18a32f52ff9da86ee53883de68d00e02
muhammad-ahmad-shafiq <39580372+muhammad-ahmad-shafiq@users.noreply.github.com>
2019-01-26 14:31:31 +0500

    21 Fix multiple subscribe bug (#3)

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :events, except: [:show]
   get '/:username/:eventname' => 'events#show', as: :show_event
   post '/new/preview' => 'events#preview', as: :event_preview
-  get '/events/:id/calendar' => 'events#calendar', as: :calendar
+  get '/events/:id/calendar:id' => 'events#calendar', as: :calendar
 
   get '/google8bd9dfe9e823ee85', :to => redirect('/google8bd9dfe9e823ee85.html')
 end

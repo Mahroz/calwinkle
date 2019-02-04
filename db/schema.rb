25a5f6f52ee564089ad08c332f62f540ec16f917
Ahmad Shafiq <mahmadshafiq786@gmail.com>
2019-02-04 22:57:19 +0500

    32 Track the viewer of an event * We are tracking the show page for this

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_192606) do
+ActiveRecord::Schema.define(version: 2019_02_04_173006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_02_03_192606) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_cancel", default: false
+    t.integer "viewer_count", default: 0
     t.index ["event_url"], name: "index_events_on_event_url", unique: true
     t.index ["user_id"], name: "index_events_on_user_id"
   end

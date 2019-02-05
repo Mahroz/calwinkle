8d5d7b77301f0b58153cc3cf86b1bf5eb13b0cc1
muhammad-ahmad-shafiq <39580372+muhammad-ahmad-shafiq@users.noreply.github.com>
2019-02-05 16:54:11 +0500

    54 Add Event Show Layout for users (#20)

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,11 @@
+// This is manifest for the users layout which will be render only for end users
+//
+//= require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require moment
+//= require tempusdominus-bootstrap-4
+//= require rails-ujs
+//= require turbolinks
+//= require jquery-ui

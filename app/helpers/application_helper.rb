d9a55ee2ccacc3f27a3305787210d36682495269
Nishant Goel <nishant@pluralcode.com>
2019-02-13 15:23:32 +0530

    ui fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def can_show_header?
+    if params[:controller] == 'events' && params[:action] == 'show'
+      false
+    else
+      true
+    end
+  end
+
+  def body_class
+    params[:controller] == 'events' && params[:action] == 'show' ? "full-width" : ''
+  end
 end

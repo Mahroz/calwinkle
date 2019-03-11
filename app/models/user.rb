4d08c4c8fc7e54e6c18f7996a4cfc83f3966979a
Nishant Goel <nishant@pluralcode.com>
2019-03-11 16:43:23 +0530

    separate table to users event cal

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,16 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   has_many :events
+  has_one :event_calendar
 
   def set_events_calendar_data
     @event_data = self.events.not_cancelled.map{|e| e.cal_data}.flatten
     @base_url = Rails.env.production? ? 'https://www.calwinkle.com/' : 'http://localhost:3000'
-    
+
     template = File.read("#{Rails.root}/app/views/events/calendar_js_template.html.erb")
     result = ERB.new(template).result(binding)
-    File.open("app/views/users/user_event_calendars/user_#{self.id}_calendar.html", 'w+') do |f|
-      f.write result
-    end
+    event_cal = self.event_calendar.present? ? self.event_calendar : self.build_event_calendar
+    event_cal.assign_attributes(calendar_html: result)
+    event_cal.save
   end
 end
